95365406aa85e2b59999e04789238e82e811e76a
aram <alexanianaram@gmail.com>
2024-04-10 12:36:49 -0400

    Added comments

--- app/ai_image_generation.py
+++ app/ai_image_generation.py
@@ -5,15 +5,21 @@ from sdkit.models import load_model
 from sdkit.generate import generate_images
 from sdkit.utils import log
 
+######################## ARGUMENT PROCESSING ###########################
+
+# Looks for an entered positive prompt (First argument)
 if(len(sys.argv) < 2):
     enteredPrompt = 'max fleischer style, a photo of a man shooting a gun'
 else:
     enteredPrompt = sys.argv[1]
     
+# Looks for an enetered negative prompt (Second argument)
 if(len(sys.argv) < 3):
     enteredNegativePrompt = ' '
 else:
     enteredNegativePrompt = sys.argv[2]
+    
+############################## MODEL SPECIFICATION ###########################
 
 context = sdkit.Context()
 
@@ -21,18 +27,24 @@ context = sdkit.Context()
 context.model_paths['stable-diffusion'] = '..\\config\\fleischerStyle_v1.ckpt'
 load_model(context, 'stable-diffusion')
 
-outputs = 1
-frameDir = "..\\originalFrames\\"
-targetDir = "..\\generatedFrames\\"
-frames = []
 
+
+############################## IMAGE GENERATION ################################
+# Number of frames to output per input
+outputs = 1
+# Directory of frames to be processed
+frameDir = "..\\originalFrames\\"
+# Target directory for proocessed frames
+targetDir = "..\\generatedFrames\\"
+
+# Loops through all frames found in the originalFrames directoryd
 for frame in os.listdir(frameDir):
     if os.path.isfile(os.path.join(frameDir, frame)) and frame.endswith((".jpg")):
-        # generate the image
+        # generates the image from the frame
         images = generate_images(context, prompt=enteredPrompt, num_outputs=outputs, negative_prompt=enteredNegativePrompt, width=550, height=550, sampler_name='dpmpp_sde', 
                          init_image=frameDir+frame, preserve_init_image_color_profile=True, prompt_strength=0.38)
 
-        # save the image
+        # save the generated image in the generatedFrames directory
 
         for i in range(0, outputs):
             saveName = targetDir + frame
