ced7455b54a84951fd8b49565cc5546caa89aa53
aram <alexanianaram@gmail.com>
2024-07-20 23:23:50 -0400

    updated cleanup function

--- app/cleanup.py
+++ app/cleanup.py
@@ -1,21 +1,14 @@
 import os
 
 def cleanup():
-    ComfyInputs = "C:\\ComfyUI_windows_portable\\ComfyUI\\input"
-    ComfyOutputs = "C:\\ComfyUI_windows_portable\\ComfyUI\\output"
-    Generated = "C:\\VideoToAnime\\generatedFrames"
-    Split = "C:\\VideoToAnime\\originalFrames"
+    Generated = "../generatedFrames/"
+    Split = "../originalFrames/"
+    Merged = "merged/"
 
-    for filename in os.listdir(ComfyInputs):
-        file_path = os.path.join(ComfyInputs, filename)
+    for filename in os.listdir(Merged):
+        file_path = os.path.join(Merged, filename)
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
-        
-    for filename in os.listdir(ComfyOutputs):
-        file_path = os.path.join(ComfyOutputs, filename)
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        
         
     for filename in os.listdir(Generated):
         file_path = os.path.join(Generated, filename)
