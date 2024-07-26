1c11e6a67a461f8b0bcd7cb42411e8c17905b37a
Chang Liu <lyalc0542@gmail.com>
2024-07-26 10:29:15 -0400

    Add audio merging to frame stitcher

--- app/frame_stitcher.py
+++ app/frame_stitcher.py
@@ -1,11 +1,21 @@
 import cv2
 import os
+from audio_merger import merge_audio_to_video
 
-def stitch_frames(videoName):
+def get_frame_rate(videoPath):
+    # Get the frame rate
+    frame_rate = cv2.VideoCapture(videoPath).get(cv2.CAP_PROP_FPS)
+    return frame_rate
+
+def stitch_frames(videoName,videoPath, audioPath):
     # Path to the directory containing frames
     frame_directory = "..\\generatedFrames\\"
+    # Temp video file to hold the video stitched
+    temp_video_file = "..\\output\\temp_"+videoName+".mov"
     # Output video file name
     output_video_file = "..\\output\\"+videoName+".mov"
+    # Frame rate from the input video
+    frame_rate = get_frame_rate(videoPath)
 
     # Get list of frame files in the directory, sorted by frame number
     frame_files = sorted([f for f in os.listdir(frame_directory) if f.endswith('.png') or f.endswith('.jpg')], key=lambda x: int(x[5:-4]))
@@ -16,7 +26,7 @@ def stitch_frames(videoName):
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Note: for .mov files, 'mp4v' codec is commonly used
-    video = cv2.VideoWriter(output_video_file, fourcc, 30.0, (width, height))
+    video = cv2.VideoWriter(temp_video_file, fourcc, frame_rate, (width, height))
 
     # Iterate over each frame file and write it to the video
     for frame_file in frame_files:
@@ -26,4 +36,13 @@ def stitch_frames(videoName):
 
     # Release the VideoWriter object
     video.release()
+    # Merge audio with the created video
+    merge_audio_to_video(temp_video_file, audioPath, output_video_file)
+    os.remove(temp_video_file)
     print("Video has been created successfully!")
+
+if __name__ == "__main__":
+    video_name = "test_video"
+    video_path = "D:\\repo\\VideoToAnime\\input\\tenSeconds.mp4"
+    audio_path = "..\\audio\\tenSeconds.mp3"
+    stitch_frames(video_name, video_path, audio_path)
